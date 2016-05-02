01efbd51bc34c5bddf4c8450567ccc7b85819659
Micah Silverman <micah@afitnerd.com>
2016-05-01 21:38:07 -0400

    refactored info text

--- src/main/java/com/stormpath/ozorkauth/controller/GameController.java
+++ src/main/java/com/stormpath/ozorkauth/controller/GameController.java
@@ -76,7 +76,8 @@ public class GameController {
             "    http POST " + baseUrl + "/v1/c Authorization:'Bearer <access token>' request='go north'",
             "Note: if you don't send the request parameter, the response will contain the result of looking around your current location in the game",
             "",
-            "Part of the game is discovering which language elements work to move you forward in the game. If you are impatient, here's a list of all the available commands: http://zork.wikia.com/wiki/Command_List"
+            "Part of the game is discovering which language elements work to move you forward in the game.",
+            "If you are impatient, here's a list of all the available commands: http://zork.wikia.com/wiki/Command_List"
         };
 
         res.setResponse(response);
